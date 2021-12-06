31628d79cc7ead6d63b17a3fa34f31149dfae408
Víctor González Prieto <victor.gp.dev@gmail.com>
2021-12-07 00:44:23 +0100

    extend day 6 solution for part 2

--- solutions/python/day06.py
+++ solutions/python/day06.py
@@ -1,17 +1,20 @@
 # https://adventofcode.com/2021/day/6
 
 from collections import deque
-from sys import stderr
+from sys import stderr, argv
 
 
-def main(input: str) -> None:
+def main(part: int, input: str) -> None:
     ntimer_values = 9  # internal timer values go from 0 to 8
     nfish_at_timers = [0] * ntimer_values
     timers = [int(timer_str) for timer_str in input.split(",")]
     for timer in timers:
         nfish_at_timers[timer] += 1
     nfish_at_timers = deque(nfish_at_timers, ntimer_values)
-    simulate(nfish_at_timers, 80)
+    if part == 1:
+        simulate(nfish_at_timers, 80)
+    else:
+        simulate(nfish_at_timers, 256)
     print(nfish_at_timers, file=stderr)
     total = sum(nfish_at_timers)
     print(total)
@@ -28,7 +31,19 @@ def simulate(nfish_at_timers: deque[int], days_left: int) -> None:
     simulate(nfish_at_timers, days_left - 1)
 
 
+bad_argc = 'Santa says: "this program uses a single argument. we give out stars on channels 1 and 2."'
+bad_arg1 = 'Santa says: "there\'s nothing here, you may want to try on channels 1 or 2."'
+
+if len(argv) != 2:  # 0 is prog_name
+    print(bad_argc, file=stderr)
+    exit(2)
+elif int(argv[1]) not in [1, 2]:
+    print(bad_arg1, file=stderr)
+    exit(2)
+
+part = int(argv[1])
+
 with open("../../input/day06-in.txt") as f:
     input = f.read()
 
-main(input)
+main(part, input)
